c0f5c4b9cbe973ff3d9fc40421bf02661862c523
moyen90 <moyen@connekt.studio>
2022-04-19 23:49:49 +0600

    header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,18 @@ import '../Style/Style.css'
 const Header = () => {
     return (
         <div className='header'>
+            <div className='nav-bar'>
+                <nav>
+                    <ul>
+                        <li><a className='link' href="#about">ABOUT</a></li>
+                        <li><a className='link' href="#skill">SKILLS</a></li>
+                        <li><a className='link' href="#projects">PROJECTS</a></li>
+                        <li><a className='link' href="#contact">CONTACT</a></li>
+                    </ul>
+                </nav>
+            </div>
             <h1>Muhammad Moyenul Islam</h1>
+            <h2>Full Stack Developer</h2>
         </div>
     );
 };
