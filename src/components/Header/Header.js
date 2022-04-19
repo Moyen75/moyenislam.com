94145107dca8bd8a0f25384707d883040cd6159f
moyen90 <moyen@connekt.studio>
2022-04-19 16:30:05 +0600

    portfolio created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import img from '../images/bg.gif'
+import '../Style/Style.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Muhammad Moyenul Islam</h1>
+        </div>
+    );
+};
+
+export default Header;
